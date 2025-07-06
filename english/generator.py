a75663ad2746211f172a5720ecd1791c83de17ae
wwuoneway <wwuoneway@gmail.com>
2025-07-06 14:37:19 -0500

    add abc lowercase

--- english/generator.py
+++ english/generator.py
@@ -5,6 +5,7 @@
 
 from worksheets.simple_1 import generate_simple_1
 from worksheets.abc_001 import generate_abc_001
+from worksheets.abc_002 import generate_abc_002
 
 
 def main():
@@ -15,6 +16,9 @@ def main():
 
     # Generate the ABC 4-line large practice worksheet
     generate_abc_001()
+
+    # Generate the abc 4-line large practice worksheet
+    generate_abc_002()
 
     print("All worksheets generated successfully.")
 
